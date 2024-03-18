d5993b83dbaa18ef6329a2cabba9431b99dbbf0b
Abdul Munif <abdul.munif@its.ac.id>
2024-03-18 17:57:22 +0800

    Updating the binning. Adding sql for importing GWS data.

---
+++ Import GWS Data.sql
@@ -0,0 +1,3 @@
+LOAD DATA INFILE 'D:/Downloads/gwasCatalog.txt' INTO TABLE gwascatalog;
+LOAD DATA INFILE 'D:/Downloads/hg38/GeneReviews/geneReviews.txt' INTO TABLE geneReviews;
+LOAD DATA INFILE 'D:/Downloads/hg38/GeneReviews/geneReviewsDetail.txt' INTO TABLE genereviewsdetail;
