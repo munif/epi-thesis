05243811aa4e35f245bb91f0394f87cf33e826e9
Abdul Munif <abdul.munif@its.ac.id>
2024-03-25 18:10:59 +0800

    Adding counting histone notebook

--- Import GWS Data.sql
+++ Import GWS Data.sql
@@ -1,3 +1,5 @@
 LOAD DATA INFILE 'D:/Downloads/gwasCatalog.txt' INTO TABLE gwascatalog;
 LOAD DATA INFILE 'D:/Downloads/hg38/GeneReviews/geneReviews.txt' INTO TABLE geneReviews;
 LOAD DATA INFILE 'D:/Downloads/hg38/GeneReviews/geneReviewsDetail.txt' INTO TABLE genereviewsdetail;
+
+LOAD DATA INFILE 'D:/Downloads/ncbiRefSeq.txt' INTO TABLE ncbirefseq;
