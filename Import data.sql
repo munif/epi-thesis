09ef0c962ceeae0e5704af3a85553f0912721c81
Abdul Munif <abdul.munif@its.ac.id>
2024-04-03 11:33:54 +0800

    Add import sql commands.

--- Import data.sql
+++ Import data.sql
@@ -2,4 +2,20 @@ LOAD DATA INFILE 'D:/Downloads/gwasCatalog.txt' INTO TABLE gwascatalog;
 LOAD DATA INFILE 'D:/Downloads/hg38/GeneReviews/geneReviews.txt' INTO TABLE geneReviews;
 LOAD DATA INFILE 'D:/Downloads/hg38/GeneReviews/geneReviewsDetail.txt' INTO TABLE genereviewsdetail;
 
-LOAD DATA INFILE 'D:/Downloads/ncbiRefSeq.txt' INTO TABLE ncbirefseq;
+LOAD DATA INFILE D:/Downloads/ncbiRefSeq.txt INTO TABLE ncbirefseq;
+
+
+mysql.exe -u root hg38 < all_mrna.sql
+mysql.exe -u root hg38 < knownGene.sql
+mysql.exe -u root hg38 < knownGeneExt.sql
+mysql.exe -u root hg38 < knownGeneMrna.sql
+mysql.exe -u root hg38 < knownToMrna.sql
+mysql.exe -u root hg38 < knownToMrnaSingle.sql
+mysql.exe -u root hg38 < mrnaOrientInfo.sql
+mysql.exe -u root hg38 < spMrna.sql
+mysql.exe -u root hg38 < tRNAs.sql
+mysql.exe -u root hg38 < wgRna.sql
+mysql.exe -u root hg38 < xenoMrna.sql
+
+
+D:/Repo/hg38/database/
