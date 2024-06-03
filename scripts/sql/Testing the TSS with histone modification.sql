23fff815afa971faf8b35fcae4efe8a2ecc2a9a6
Abdul Munif <munif@if.its.ac.id>
2024-06-04 00:24:06 +0800

    - Adding testing folder - Adding encoding data using numpy and pandas. At the end, I choose pandas which more efficient.

--- scripts/sql/Testing the TSS with histone modification.sql
+++ scripts/sql/Testing the TSS with histone modification.sql
@@ -1,13 +1,13 @@
 SELECT 	h_chrom, h_chromStart, h_chromEnd, n_name2 , tss, tss_start, tss_end, h_value_1, 
 		H3K4me3 , H3K9ac , H3K9me3 , H3K27ac, H3K27me3 
-FROM histone_count_overlap80 hco
+FROM common.histone_count_overlap80 hco
 WHERE tss  = 17721238
 
 -- WHERE H3K4me3 >= 10
 -- ORDER BY H3K4me3 DESC, h_value_1 DESC 
 
 SELECT min(h_value_1), max(h_value_1)
-FROM histone_count_overlap80 hco 
+FROM common.histone_count_overlap80 hco 
 
 SELECT * -- , chromEnd - chromStart  AS length
 FROM common.h3k4me3 hkm
@@ -32,3 +32,35 @@ SELECT 	H3K4me3 + H3K9ac + H3K27ac AS T,
 		H3K4me3 AS A,
 		h_value_1  AS value_1
 FROM common.histone_count_overlap80 hco
+
+
+-- Find the histone marker
+SELECT 	h_chrom,
+		tss,
+		h_chromStart,
+		h_chromEnd,
+		h_value_1,
+		H3K4me3,
+		H3K9ac,
+		H3K9me3,
+		H3K27ac,
+		H3K27me3,
+		histone_total_count 
+FROM common.histone_count_overlap80 hco 
+WHERE tss = 17721238
+-- tss = 9294832
+
+SELECT *
+FROM common.h3k4me3 hkm
+WHERE chrom = 'chr1'
+AND (
+((chromStart >= 9294832 - 2000) AND (chromEnd <= 9294832 + 2000))
+OR
+((chromStart <= 9294832 - 2000) AND (chromEnd <= 9294832 + 2000) AND (chromEnd >= 9294832 - 2000) AND (((chromEnd - (9294832 - 2000))/146) >= 0.8))
+)
+
+
+SELECT *
+FROM histone_count_overlap80 
+WHERE h_chrom = 'chr1'
+AND h_chromStart = 24741636
