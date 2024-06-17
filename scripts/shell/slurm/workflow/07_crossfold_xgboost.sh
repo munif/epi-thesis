e3d3f660746d6a595e2be3b579500af2db3ca739
Abdul Munif <munif@if.its.ac.id>
2024-06-17 18:10:22 +0800

    Adding: - Find the XGBoost best parameter using GridSearch - Chromosome-fold Validation - k-fold Validation

--- scripts/shell/slurm/workflow/07_crossfold_xgboost.sh
+++ scripts/shell/slurm/workflow/07_crossfold_xgboost.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=xgboost
-#SBATCH --output=slurm-%j.out
-#SBATCH --error=slurm-%j.err
+#SBATCH --job-name=xgboost-cross-fold
+#SBATCH --output=slurm-%x-%j.out
+#SBATCH --error=slurm-%x-%j.err
 #SBATCH --nodes=4
 #SBATCH --partition=work
-#SBATCH --ntasks=32
-#SBATCH --nodelist=n[011-013,024-025,028-031,033-034]
+#SBATCH --ntasks=64
+#SBATCH --nodelist=n[010-013,024,028-031,033-034]
 #SBATCH --time=2-00:00:00
 #SBATCH --mem=200GB
 
@@ -13,7 +13,7 @@
 start_time=$(date +%s.%N)
 
 # Run Python script
-conda run -n epi-thesis python /group/pmc021/amunif/epi-thesis/scripts/shell/slurm/workflow/04_xgboost_best_param.py
+conda run -n epi-thesis python /group/pmc021/amunif/epi-thesis/scripts/shell/slurm/workflow/07_crossfold_xgboost.py
 
 # Record end time
 end_time=$(date +%s.%N)
