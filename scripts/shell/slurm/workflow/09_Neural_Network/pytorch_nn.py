b61d07719117dffd6fcfe3b10d41c835b8aa352b
Abdul Munif <munif@if.its.ac.id>
2024-07-02 17:13:26 +0800

    - Adding fully connected layer and convolutional 1D using Pytorch

--- scripts/shell/slurm/workflow/09_Neural_Network/pytorch_nn.py
+++ scripts/shell/slurm/workflow/09_Neural_Network/pytorch_nn.py
@@ -1,12 +1,7 @@
-
-
-
-!pwd
-
-
 # Load the required library
 import pandas as pd
 import numpy as np
+from datetime import datetime
 
 import torch
 import torch.nn as nn
@@ -16,11 +11,11 @@ from torch.utils.data import DataLoader, TensorDataset
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
-
-progress_file = '/group/pmc021/amunif/epi-thesis/workflow/04. Neural Network/progress.txt'
-
-
 dataset_path = "/group/pmc021/amunif/epi-thesis/dataset/"
+
+current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+progress_file = f"/group/pmc021/amunif/epi-thesis/scripts/shell/slurm/workflow/output/09/progress_{current_time}.txt"
+
 
 def load_large_csv(file_name, chunksize=20000):
     # Read the CSV file
@@ -34,26 +29,20 @@ def load_large_csv(file_name, chunksize=20000):
     del mylist
     return df
 
-
 def save_progress(file_name, message):
     with open(file_name, 'a+') as file:
         file.write(message + "\n")
 
-
 # Load the data
-X = pd.read_csv(f"{dataset_path}histone_features.csv", nrows=100)
-y = pd.read_csv(f"{dataset_path}value_1_df.csv", nrows=100)
-
+X = load_large_csv(f"{dataset_path}histone_features.csv")
+y = load_large_csv(f"{dataset_path}value_1_df.csv")
 
 # Convert to numpy
 X_np = X.to_numpy()
 y_np = y.to_numpy()
 
-
 # Split the data into training and testing
 X_train_np, X_test_np, y_train_np, y_test_np = train_test_split(X_np, y_np, test_size=0.2, random_state=42)
-
-
 
 # Convert NumPy arrays to PyTorch tensors
 X_train_tensor = torch.tensor(X_train_np, dtype=torch.float32)
@@ -61,45 +50,39 @@ y_train_tensor = torch.tensor(y_train_np, dtype=torch.float32)
 X_test_tensor = torch.tensor(X_test_np, dtype=torch.float32)
 y_test_tensor = torch.tensor(y_test_np, dtype=torch.float32)
 
-
 # Create TensorDataset for training and testing sets
 train_dataset = TensorDataset(X_train_tensor, y_train_tensor)
 test_dataset = TensorDataset(X_test_tensor, y_test_tensor)
-
 
 # Create a DataLoader
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 
-
 # Neural network model
-
 class NeuralNetwork(nn.Module):
     def __init__(self, input_dim):
         super(NeuralNetwork, self).__init__()
-        self.fc1 = nn.Linear(input_dim, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, 128)
-        self.fc4 = nn.Linear(128, 1)  # Single output for regression
+        self.fc1 = nn.Linear(input_dim, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 256)
+        self.fc4 = nn.Linear(256, 128)
+        self.fc5 = nn.Linear(128, 1)  # Single output for regression
 
     def forward(self, x):
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
         x = torch.relu(self.fc3(x))
-        x = self.fc4(x)  # No activation function for the output layer in regression
+        x = torch.relu(self.fc4(x))
+        x = self.fc5(x)  # No activation function for the output layer in regression
         return x
-
-
+    
 input_dim = 20000
 model = NeuralNetwork(input_dim)
 criterion = nn.MSELoss()  # Mean Squared Error loss for regression
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-
 # Train the model
 num_epochs = 100
-
-
 for epoch in range(num_epochs):
     model.train()
     running_loss = 0.0
@@ -114,7 +97,6 @@ for epoch in range(num_epochs):
     message = f'Epoch [{epoch + 1}/{num_epochs}], Loss: {running_loss/len(train_loader):.4f}'
     print(message)
     save_progress(progress_file, message)
-
 
 # Evaluate the model
 model.eval()
@@ -132,11 +114,9 @@ print(f'Average loss on the training data: {total_loss/len(train_loader):.4f}')
 with torch.no_grad():
     predictions = model(X_test_tensor)
 
-
 # Convert predictions to NumPy array
 predictions_np = predictions.numpy()
 y_test_np = y_test_tensor.numpy()
-
 
 # Calculate evaluation metrics
 mse = mean_squared_error(y_test_np, predictions_np)
@@ -149,7 +129,6 @@ print(f'RMSE: {rmse}')
 print(f'MAE: {mae}')
 print(f'R2 Score: {r2}')
 
-
 # Create a DataFrame with true values and predictions
 results_df = pd.DataFrame({
     'True Values': y_test_np.flatten(),
@@ -157,6 +136,5 @@ results_df = pd.DataFrame({
 })
 
 # Save the DataFrame to a CSV file
-results_df.to_csv('predictions.csv', index=False)
-
-print("Predictions and true values saved to 'predictions.csv'.")
+results_df.to_csv(f"predictions_{current_time}.csv", index=False)
+print(f"Predictions and true values saved to 'predictions_{current_time}.csv'.")
