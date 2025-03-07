beb9e45c95578b728f6345a197eaff3572a8f870
Abdul Munif <munif@if.its.ac.id>
2025-03-07 08:48:50 +0800

    Adding autoencoder and do the experiments for E066 Dataset

---
+++ workflow/09_Learning_to_Rank/slurm/autoencoder/autoencoder-linear.py
@@ -0,0 +1,498 @@
+import sys
+
+sys.path.insert(0, '/group/pmc021/amunif/epi-thesis/workflow/09_Learning_to_Rank/notebook/mylib')
+
+import os
+import random
+from datetime import datetime
+
+import pandas as pd
+import polars as pl
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.optim.lr_scheduler as lr_scheduler
+from torch.utils.data import Dataset, DataLoader, TensorDataset
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score, accuracy_score, roc_curve, auc, precision_recall_fscore_support, confusion_matrix, classification_report
+from sklearn.metrics import ConfusionMatrixDisplay
+
+import BinaryClassifier as BC
+import HepG2Dataset as HG2D
+import my_util
+# import AutoEncoder as AE
+
+class LinearAutoEncoder(nn.Module):
+    def __init__(self, input_dim=20000, bottleneck_dim=100):
+        super(LinearAutoEncoder, self).__init__()
+        
+        # Encoder
+        self.encoder = nn.Sequential(
+            # First layer
+            nn.Linear(input_dim, 1024),
+            nn.BatchNorm1d(1024),
+            nn.ReLU(),
+            
+            # Second layer
+            nn.Linear(1024, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            
+            # Third layer
+            nn.Linear(512, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            
+            # Final layer
+            nn.Linear(256, bottleneck_dim)
+        )
+        
+        # Decoder
+        self.decoder = nn.Sequential(
+            # First layer
+            nn.Linear(bottleneck_dim, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            
+            # Second layer
+            nn.Linear(256, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            
+            # Third layer
+            nn.Linear(512, 1024),
+            nn.BatchNorm1d(1024),
+            nn.ReLU(),
+            
+            # Final layer
+            nn.Linear(1024, input_dim)
+        )
+    
+    def forward(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
+    
+    def encode(self, x):
+        return self.encoder(x)
+    
+    def decode(self, x):
+        return self.decoder(x)
+
+# Declare  variables, working directory and dataset directory
+MODE = 'all_features'
+NUM_EPOCHS = 100
+START_EPOCH = 0
+NUM_ITEMS = 1_000_000
+BATCH_SIZE = 64
+DROPOUT_RATE = 0.2
+
+L1_lambda = 0.005
+L2_lambda = 0.001
+# L1_lambda = 0.0
+# L2_lambda = 0.0
+
+WORKING_DIR = '/group/pmc021/amunif/epi-thesis/workflow/09_Learning_to_Rank'
+DATASET_DIR = '/group/pmc021/amunif/epi-thesis/workflow/09_Learning_to_Rank/dataset'
+
+MODEL_MODE = "dol1l2" # regular (reg), dropout (do), dropout + l1l2 (dol1l2)
+PREFIX = f"{MODEL_MODE}-autoencoder-linear-{DROPOUT_RATE}"
+SUFFIX = f"{NUM_ITEMS}-{NUM_EPOCHS}-{BATCH_SIZE}-{L1_lambda}-{L2_lambda}"
+
+CHECKPOINT_NAME = os.path.join(WORKING_DIR, 'output', 'autoencoder', MODE, 'model', f'{PREFIX}-checkpoint-{SUFFIX}.pt')
+
+IMAGE_FILE = os.path.join(WORKING_DIR, 'output', 'autoencoder', MODE, 'img', 'train', f"{PREFIX}-ranking-{SUFFIX}.png")
+TEST_FILE = os.path.join(WORKING_DIR, 'output', 'autoencoder', MODE, 'test', f"{PREFIX}-test-results-{SUFFIX}.txt")
+TRAINING_FILE = os.path.join(WORKING_DIR, 'output', 'autoencoder', MODE, "train", f"{PREFIX}-train-validation-metrics-{SUFFIX}.csv")
+TEST_RESULT_FILE = os.path.join(WORKING_DIR, 'output', 'autoencoder', MODE, "test", f"{PREFIX}-test-metrics-{SUFFIX}.csv")
+ROC_FILE = os.path.join(WORKING_DIR, 'output', 'autoencoder', MODE, 'img', 'roc', f"{PREFIX}-roc-{SUFFIX}.png")
+CM_FILE = os.path.join(WORKING_DIR, 'output', 'autoencoder', MODE, 'img', 'cm', f"{PREFIX}-cm-{SUFFIX}.png")
+
+INPUT_SIZE = 256
+
+print(CHECKPOINT_NAME)
+
+device = my_util.get_device()
+
+# Load dataset
+all_features = pl.read_parquet(os.path.join(DATASET_DIR, 'features', f'{MODE}.parquet'))
+all_features_np = all_features.to_numpy()
+
+# Preparing for autoencoder
+X_col = all_features_np[:, 2]
+y_col = all_features_np[:, 3]
+
+X_col_2d = np.vstack(X_col)
+
+# Split dataset
+X_train, X_test, y_train, y_test = train_test_split(X_col, y_col, test_size=0.2, random_state=1)
+
+# Convert the Numpy object to Numpy array
+X_train_2d = np.vstack(X_train)
+X_test_2d = np.vstack(X_test)
+
+# Convert to PyTorch tensors
+X_train_tensor = torch.tensor(X_train_2d, dtype=torch.float32).to(device)
+X_test_tensor = torch.tensor(X_test_2d, dtype=torch.float32).to(device)
+
+n_inputs = X_train_2d.shape[1]
+
+# Initialize model
+n_bottleneck = 128
+ae_model = LinearAutoEncoder(n_inputs, n_bottleneck).to(device)
+
+# Define loss function and optimizer
+criterion = nn.MSELoss()
+optimizer = optim.Adam(ae_model.parameters())
+
+# Training loop
+n_epochs = 200
+batch_size = 32
+train_losses = []
+test_losses = []
+
+for epoch in range(n_epochs):
+    ae_model.train()
+    train_loss = 0
+    
+    # Train in batches
+    for i in range(0, len(X_train), batch_size):
+        batch_X = X_train_tensor[i:i + batch_size]
+        
+        # Forward pass
+        outputs = ae_model(batch_X)
+        loss = criterion(outputs, batch_X)
+        
+        # Backward pass and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        train_loss += loss.item()
+    
+    # Evaluate on test set
+    ae_model.eval()
+    with torch.no_grad():
+        test_outputs = ae_model(X_test_tensor)
+        test_loss = criterion(test_outputs, X_test_tensor)
+    
+    train_losses.append(train_loss / (len(X_train) / batch_size))
+    test_losses.append(test_loss.item())
+    
+    # if (epoch + 1) % 10 == 0:
+    print(f'Epoch [{epoch+1}/{n_epochs}], Train Loss: {train_losses[-1]:.4f}, Test Loss: {test_losses[-1]:.4f}')
+
+torch.save(ae_model.state_dict(), 'autoencoder-linear.pth')
+
+ae_model = LinearAutoEncoder(n_inputs, n_bottleneck).to(device)
+ae_model.load_state_dict(torch.load('autoencoder-linear.pth'))
+
+X_col_tensor = torch.tensor(X_col_2d, dtype=torch.float32).to(device)
+
+ae_model.eval()
+with torch.no_grad():
+    X_encoded = ae_model.encode(X_col_tensor)
+
+
+X_encoded_np = X_encoded.cpu().detach().numpy()
+X_encoded_pl = pl.from_numpy(X_encoded_np, schema=["all_features_encoded"])
+# Concatenate the new column horizontally
+af_encoded = pl.concat([all_features, X_encoded_pl], how='horizontal')
+af_encoded_np = af_encoded.to_numpy()
+
+
+# Split train, test, validation by index
+data_indices = np.arange(len(af_encoded_np))
+
+# First split: 80% train, 20% temporary (for test + validation)
+train_idx, temp_idx = train_test_split(
+    data_indices, 
+    test_size=0.2, 
+    random_state=42  # For reproducibility
+)
+
+# Second split: Split temp_idx into 50% test and 50% validation
+val_idx, test_idx = train_test_split(
+    temp_idx, 
+    test_size=0.5, 
+    random_state=42  # Same random_state for consistency
+)
+
+# Define the dataset
+train_dataset = HG2D.HepG2DatasetAutoEncoder(int(0.8 * NUM_ITEMS), af_encoded_np, train_idx)
+val_dataset = HG2D.HepG2DatasetAutoEncoder(int(0.1 * NUM_ITEMS), af_encoded_np, val_idx)
+test_dataset = HG2D.HepG2DatasetAutoEncoder(int(0.1 * NUM_ITEMS), af_encoded_np, test_idx)
+
+# Define the dataloader
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, pin_memory=True)
+val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=True, pin_memory=True)
+test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True, pin_memory=True)
+
+# Initialize the model
+model = BC.BinaryClassifierDropOutL1L2(input_size=INPUT_SIZE, dropout_rate=DROPOUT_RATE, l1_lambda=L1_lambda, l2_lambda=L2_lambda).to(device)
+
+# Define loss function for binary classification
+criterion = nn.BCELoss()  # Binary Cross Entropy Loss
+
+# Training loop example
+optimizer = optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
+
+# Define learning rate scheduler
+scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, patience=5, factor=0.1)
+
+print(model)
+
+# Arrays for saving the performance metric
+train_losses, val_losses = [], []
+train_accuracies, val_accuracies = [], []
+train_aucs, val_aucs = [], []
+val_precisions = []
+val_recalls = []
+val_f1_scores = []
+
+# Check if the checkpoint exists
+if os.path.exists(CHECKPOINT_NAME):
+    checkpoint = torch.load(CHECKPOINT_NAME)
+    
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    scheduler.load_state_dict(checkpoint['scheduler'])
+    START_EPOCH = checkpoint['epoch'] + 1
+    train_losses = checkpoint['train_losses']
+    val_losses = checkpoint['val_losses']
+    train_accuracies = checkpoint['train_accuracies']
+    val_accuracies = checkpoint['val_accuracies']
+    train_aucs = checkpoint['train_aucs']
+    val_aucs = checkpoint['val_aucs']
+    val_precisions = checkpoint['val_precisions']
+    val_recalls = checkpoint['val_recalls']
+    val_f1_scores = checkpoint['val_f1_scores']
+
+    print(f"{CHECKPOINT_NAME} exists. Resuming training from epoch {START_EPOCH}.")
+else:
+    START_EPOCH = 0
+
+# Training loop
+for epoch in range(START_EPOCH, NUM_EPOCHS):
+    model.train()
+
+    train_loss = 0
+    train_correct = 0
+    train_total = 0
+    train_true_labels = []
+    train_predicted_labels = []
+
+    # Training phase
+    for X_batch, y_batch in train_loader:
+
+        if (MODEL_MODE == "reg") or (MODEL_MODE == "do"):
+            outputs = model(X_batch.to(dtype=torch.float32).to(device))
+            loss = criterion(outputs.squeeze(1), y_batch.to(dtype=torch.float32).to(device))
+        elif MODEL_MODE == "dol1l2":
+            outputs, l1_reg, l2_reg = model(X_batch.to(dtype=torch.float32).to(device))
+            loss = criterion(outputs.squeeze(1), y_batch.to(dtype=torch.float32).to(device)) + l1_reg + l2_reg
+
+        # Backward pass and optimization
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # Calculate accuracy
+        predictions = (outputs.squeeze(1) >= 0.5).long()
+
+        train_loss += loss.item()
+        train_correct += (predictions == y_batch.to(device)).sum().item()
+        train_total += y_batch.size(0)
+        
+        train_true_labels.extend(y_batch.cpu().numpy())
+        train_predicted_labels.extend(predictions.cpu().numpy())
+        
+    # # Adjust learning rate
+    # scheduler.step()
+    
+    avg_train_loss = train_loss / len(train_loader)
+    train_accuracy = 100 * train_correct / train_total
+    train_auc_score = roc_auc_score(train_true_labels, train_predicted_labels)
+
+    train_losses.append(round(avg_train_loss, 4))
+    train_accuracies.append(round(train_accuracy, 2))
+    train_aucs.append(round(train_auc_score, 2))
+
+    # Validation phase
+    model.eval()
+    val_loss = 0.0
+    val_correct = 0
+    val_total = 0
+    val_metric = 0
+    val_true_labels = []
+    val_predicted_labels = []
+
+    with torch.no_grad():
+        for X_batch, y_batch in val_loader:
+            
+            if (MODEL_MODE == "reg") or (MODEL_MODE == "do"):
+                outputs = model(X_batch.to(dtype=torch.float32).to(device))
+                loss = criterion(outputs.squeeze(1), y_batch.to(dtype=torch.float32).to(device))
+            elif MODEL_MODE == "dol1l2":
+                outputs, l1_reg, l2_reg = model(X_batch.to(dtype=torch.float32).to(device))
+                loss = criterion(outputs.squeeze(1), y_batch.to(dtype=torch.float32).to(device)) + l1_reg + l2_reg
+            
+            predictions = (outputs.squeeze(1) >= 0.5).long()
+            
+            val_loss += loss.item()
+            val_correct += (predictions == y_batch.to(dtype=torch.float32).to(device)).sum().item()
+            val_total += y_batch.size(0)
+            val_true_labels.extend(y_batch.cpu().numpy())
+            val_predicted_labels.extend(predictions.cpu().numpy())
+            
+        avg_val_loss = val_loss / len(val_loader)
+        val_accuracy = 100 * val_correct / val_total
+        val_metric = val_correct / len(val_loader)
+
+        # Calculate precision, recall, and F1-score
+        precision, recall, f1_score, _ = precision_recall_fscore_support(
+            val_true_labels, 
+            val_predicted_labels, 
+            average='binary'  # Use macro average for multi-class classification
+        )
+
+        val_true_labels = np.array(val_true_labels)
+        val_predicted_labels = np.array(val_predicted_labels)
+        val_auc_score = roc_auc_score(val_true_labels, val_predicted_labels)
+
+        val_losses.append(round(avg_val_loss, 4))
+        val_accuracies.append(round(val_accuracy, 2))
+        val_aucs.append(round(val_auc_score, 2))
+        val_precisions.append(round(precision, 2))
+        val_recalls.append(round(recall, 2))
+        val_f1_scores.append(round(f1_score, 2))
+
+    # Adjust learning rate
+    scheduler.step(val_loss)
+
+    print(f'Epoch [{epoch+1}/{NUM_EPOCHS}] - '
+          f'[TRAIN] Loss: {avg_train_loss:.4f}, Accuracy: {train_accuracy:.2f}%, AUC: {train_auc_score:.2f} '
+          f'[VAL] Loss: {avg_val_loss:.4f}, Accuracy: {val_accuracy:.2f}%, '
+          f'Precision: {precision:.2f}, Recall: {recall:.2f}, F1-Score: {f1_score:.2f}, '
+          f'AUC: {val_auc_score:.2f}')
+    
+    # Save the model
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'scheduler': scheduler.state_dict(),
+        'train_losses': train_losses,
+        'val_losses': val_losses,
+        'train_accuracies': train_accuracies,
+        'val_accuracies': val_accuracies,
+        'train_aucs': train_aucs,
+        'val_aucs': val_aucs,
+        'val_precisions': val_precisions,
+        'val_recalls': val_recalls,
+        'val_f1_scores': val_f1_scores,
+    }, CHECKPOINT_NAME)
+
+
+# Plot metrics
+my_util.plot_metrics(train_losses, val_losses, train_accuracies, val_accuracies, NUM_ITEMS, IMAGE_FILE)
+
+# Predict on test set
+test_predictions = []
+test_true_labels = []
+
+with torch.no_grad():
+    for X_batch, y_batch in test_loader:
+        if (MODEL_MODE == "reg") or (MODEL_MODE == "do"):
+            outputs = model(X_batch.to(dtype=torch.float32).to(device))
+        elif MODEL_MODE == "dol1l2":
+            outputs, _, _ = model(X_batch.to(dtype=torch.float32).to(device))
+        
+        predictions = (outputs.squeeze(1) >= 0.5).long()
+        test_true_labels.extend(y_batch.cpu().numpy())
+        test_predictions.extend(predictions.cpu().numpy())
+
+
+# Evaluation metrics
+# Precision, Recall, F1-Score
+precision, recall, f1_score, _ = precision_recall_fscore_support(
+    test_true_labels, 
+    test_predictions, 
+    average='binary'  # Use 'macro' for multi-class
+)
+
+test_acc_score = accuracy_score(test_true_labels, test_predictions) * 100
+test_auc_score = roc_auc_score(test_true_labels, test_predictions)
+cr = classification_report(test_true_labels, test_predictions)
+cm = confusion_matrix(test_true_labels, test_predictions)
+
+test_output = f"""Accuracy Score on test set: {test_acc_score:.2f} %
+AUC Score on test set: {test_auc_score:.2f}
+Precision: {precision:.4f}
+Recall: {recall:.4f}
+F1-Score: {f1_score:.4f}
+
+Detailed Classification Report:
+{cr}
+
+Confusion Matrix:
+{cm}
+"""
+
+print(test_output)
+
+# Save the test output to text file
+with open(TEST_FILE, 'w', newline = '') as text_file:
+    text_file.write(test_output)
+
+# Training metrics
+experiment_results = {
+    'train_loss'    : train_losses,
+    'train_accuracy': train_accuracies,
+    'train_auc'     : train_aucs,
+    'val_loss'      : val_losses,
+    'val_accuracy'  : val_accuracies,
+    'val_auc'       : val_aucs
+}
+
+experiment_pl = pl.DataFrame(experiment_results)
+experiment_pl.write_csv(TRAINING_FILE)
+
+# Test output
+test_results = {
+    'test_acc'    : round(test_acc_score, 4),
+    'test_auc'    : round(test_auc_score, 4)
+}
+
+test_pl = pl.DataFrame(test_results)
+test_pl.write_csv(TEST_RESULT_FILE)
+
+# Calculate ROC curve
+fpr, tpr, thresholds = roc_curve(test_true_labels, test_predictions) 
+roc_auc = auc(fpr, tpr)
+
+# Plot the ROC curve
+plt.figure()  
+plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+plt.plot([0, 1], [0, 1], 'k--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title(f'ROC for HepG2 Ranking')
+plt.legend()
+
+plt.savefig(ROC_FILE)
+
+plt.show()
+
+# Plot the confusion matrix
+cm_display = ConfusionMatrixDisplay(confusion_matrix=cm)
+fig, ax = plt.subplots(figsize=(10, 8))
+cm_display.plot(ax=ax)
+plt.savefig(CM_FILE)
